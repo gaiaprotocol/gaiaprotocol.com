c51b469b62dde94fc5fd2bbe9a3c3646fc018a48
yjgaia <yjdev88@gmail.com>
2024-07-17 18:45:42 +0900

    Update entrypoint.ts

--- server/entrypoint.ts
+++ server/entrypoint.ts
@@ -3,7 +3,11 @@ import { serveFile } from "https://deno.land/std@0.223.0/http/file_server.ts";
 
 Deno.serve(async (req) => {
   const path = new URL(req.url).pathname;
+  console.log(path);
+
   const filePath = `${Deno.cwd()}/public${path}`;
+  console.log(filePath);
+
   if (path !== "/" && await exists(filePath)) {
     return await serveFile(req, filePath);
   } else {
