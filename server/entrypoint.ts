35a1d5a55b0b38a8b899bcc32a035b72baa273db
yjgaia <yjdev88@gmail.com>
2024-09-21 16:08:26 +0900

    Add server files and implement home page route

--- server/entrypoint.ts
+++ server/entrypoint.ts
@@ -1,16 +1,16 @@
 import { exists } from "https://deno.land/std@0.223.0/fs/mod.ts";
 import { serveFile } from "https://deno.land/std@0.223.0/http/file_server.ts";
+import { home } from "./pages/home.ts";
 
 Deno.serve(async (req) => {
   const path = new URL(req.url).pathname;
-  console.log(path);
-
   const filePath = `${Deno.cwd()}/public${path}`;
-  console.log(filePath);
 
   if (path !== "/" && await exists(filePath)) {
     return await serveFile(req, filePath);
+  } else if (path === "/") {
+    return new Response(home(), { status: 200 });
   } else {
-    return await serveFile(req, `${Deno.cwd()}/public/index.html`);
+    return new Response("Not Found", { status: 404 });
   }
 });
