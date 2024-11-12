23b7ae4b77b7471fe794be9f1bb41e85331aa7bc
yjgaia <yjdev88@gmail.com>
2024-11-12 15:15:54 +0900

    Update import paths to use refs/heads/main for consistency across modules

--- server/entrypoint.ts
+++ server/entrypoint.ts
@@ -1,6 +1,7 @@
 import { serveFile } from "https://deno.land/std@0.223.0/http/file_server.ts";
-import { corsHeaders } from "https://raw.githubusercontent.com/yjgaia/deno-module/main/api.ts";
+import { corsHeaders } from "https://raw.githubusercontent.com/yjgaia/deno-module/refs/heads/main/api.ts";
 import { pages } from "./pages.ts";
+
 Deno.serve(async (req) => {
   const basePath = Deno.cwd() + "/public";
   const path = new URL(req.url).pathname;
