0a17e250f8e408b4517d10a363b1927c348aa26a
yjgaia <yjdev88@gmail.com>
2024-11-20 14:27:05 +0900

    Add notice view styles and update notice page rendering for improved layout

--- server/pages/notice.ts
+++ server/pages/notice.ts
@@ -1,7 +1,18 @@
-import { el } from "https://raw.githubusercontent.com/yjgaia/deno-module/refs/heads/main/page.ts";
 import { marked } from "https://deno.land/x/marked@1.0.2/mod.ts";
-import { createSupabaseClient } from "./supabase.ts";
+import { el } from "https://raw.githubusercontent.com/yjgaia/deno-module/refs/heads/main/page.ts";
 import Notice from "../entities/Notice.ts";
+import { createSupabaseClient } from "./supabase.ts";
+
+function format(date: Date) {
+  return new Intl.DateTimeFormat(
+    "en",
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    },
+  ).format(date);
+}
 
 export async function notice(slug: string) {
   slug = slug.replace(/\/$/, "");
@@ -15,6 +26,15 @@ export async function notice(slug: string) {
 
   const notice = data?.[0] as Notice | undefined;
   return notice
-    ? el(".notice-view", notice.title, marked(notice.content))
+    ? el(
+      ".notice-view",
+      el(
+        "header",
+        el(".date", format(new Date(notice.created_at))),
+        el("h1", notice.title),
+        el("h2", notice.subtitle),
+      ),
+      el("main.markdown-body", marked(notice.content)),
+    )
     : el(".notice-view", "Not Found");
 }
