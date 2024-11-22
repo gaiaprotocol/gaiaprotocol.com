1d09e32b3f641a5b3c12c210ed3eec030604250d
yjgaia <yjdev88@gmail.com>
2024-11-22 18:14:17 +0900

    Add new dependencies and implement alert dialog for coming soon features

--- app/init.ts
+++ app/init.ts
@@ -1,7 +1,25 @@
 import { SPAInitializer } from "@common-module/app";
 import AppConfig, { IAppConfig } from "./AppConfig.js";
+import { AlertDialog } from "@common-module/app-components";
+import { ComingSoonIcon } from "@gaiaprotocol/svg-icons";
 
 export default async function init(config: IAppConfig) {
   AppConfig.init(config);
   SPAInitializer.init();
+
+  const comingSoonElements = document.querySelectorAll(
+    "[onclick=\"alert('Coming soon!')\"]",
+  );
+
+  comingSoonElements.forEach((element) => {
+    element.removeAttribute("onclick");
+    element.addEventListener("click", (e) => {
+      e.preventDefault();
+      new AlertDialog({
+        icon: new ComingSoonIcon(),
+        title: "Coming soon",
+        message: "This feature is coming soon.",
+      });
+    });
+  });
 }
